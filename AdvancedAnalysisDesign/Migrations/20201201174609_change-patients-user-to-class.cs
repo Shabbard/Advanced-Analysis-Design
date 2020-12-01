aa4d68114170711d42d90520bf20e6bf62ba168e
Adam Sheppard <t7.shab@gmail.com>
2020-12-01 17:47:15 +0000

    Replaced userid int with user class

---
+++ AdvancedAnalysisDesign/Migrations/20201201174609_change-patients-user-to-class.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AdvancedAnalysisDesign.Migrations
+{
+    public partial class changepatientsusertoclass : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Patients",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Patients_UserId",
+                table: "Patients",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Patients_Users_UserId",
+                table: "Patients",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Patients_Users_UserId",
+                table: "Patients");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Patients_UserId",
+                table: "Patients");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Patients",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
