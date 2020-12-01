93d3b022b7b759136645f8d1a0bfee7a0ae2aa94
Adam Sheppard <t7.shab@gmail.com>
2020-12-01 14:47:46 +0000

    Improvements based on feedback

---
+++ AdvancedAnalysisDesign/Migrations/20201201144704_EditTablesToMatchSchema.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AdvancedAnalysisDesign.Migrations
+{
+    public partial class EditTablesToMatchSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BloodworkTests_Bloodworks_BloodworkTest",
+                table: "BloodworkTests");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_UserTypes_UserTypeId",
+                table: "Users");
+
+            migrationBuilder.DropTable(
+                name: "PatientPrescriptions");
+
+            migrationBuilder.DropTable(
+                name: "UserTypes");
+
+            migrationBuilder.DropTable(
+                name: "Prescriptions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_UserTypeId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UserTypeId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "RequiresPrescription",
+                table: "BloodworkTests");
+
+            migrationBuilder.RenameColumn(
+                name: "BloodworkTest",
+                table: "BloodworkTests",
+                newName: "BloodworkId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_BloodworkTests_BloodworkTest",
+                table: "BloodworkTests",
+                newName: "IX_BloodworkTests_BloodworkId");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserType",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DateOfBirth",
+                table: "UserDetails",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "VerificationImage",
+                table: "Patients",
+                type: "varbinary(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PatientMedicationId",
+                table: "Bloodworks",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Medications",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BloodworkRequired = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medications", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PatientMedications",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PatientId = table.Column<int>(type: "int", nullable: true),
+                    MedicationId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PatientMedications", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PatientMedications_Medications_MedicationId",
+                        column: x => x.MedicationId,
+                        principalTable: "Medications",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_PatientMedications_Patients_PatientId",
+                        column: x => x.PatientId,
+                        principalTable: "Patients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bloodworks_PatientMedicationId",
+                table: "Bloodworks",
+                column: "PatientMedicationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PatientMedications_MedicationId",
+                table: "PatientMedications",
+                column: "MedicationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PatientMedications_PatientId",
+                table: "PatientMedications",
+                column: "PatientId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bloodworks_PatientMedications_PatientMedicationId",
+                table: "Bloodworks",
+                column: "PatientMedicationId",
+                principalTable: "PatientMedications",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BloodworkTests_Bloodworks_BloodworkId",
+                table: "BloodworkTests",
+                column: "BloodworkId",
+                principalTable: "Bloodworks",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bloodworks_PatientMedications_PatientMedicationId",
+                table: "Bloodworks");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_BloodworkTests_Bloodworks_BloodworkId",
+                table: "BloodworkTests");
+
+            migrationBuilder.DropTable(
+                name: "PatientMedications");
+
+            migrationBuilder.DropTable(
+                name: "Medications");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Bloodworks_PatientMedicationId",
+                table: "Bloodworks");
+
+            migrationBuilder.DropColumn(
+                name: "UserType",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "DateOfBirth",
+                table: "UserDetails");
+
+            migrationBuilder.DropColumn(
+                name: "VerificationImage",
+                table: "Patients");
+
+            migrationBuilder.DropColumn(
+                name: "PatientMedicationId",
+                table: "Bloodworks");
+
+            migrationBuilder.RenameColumn(
+                name: "BloodworkId",
+                table: "BloodworkTests",
+                newName: "BloodworkTest");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_BloodworkTests_BloodworkId",
+                table: "BloodworkTests",
+                newName: "IX_BloodworkTests_BloodworkTest");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserTypeId",
+                table: "Users",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "RequiresPrescription",
+                table: "BloodworkTests",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "Prescriptions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BloodworkId = table.Column<int>(type: "int", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prescriptions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Prescriptions_Bloodworks_BloodworkId",
+                        column: x => x.BloodworkId,
+                        principalTable: "Bloodworks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PatientPrescriptions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PatientPrescription = table.Column<int>(type: "int", nullable: true),
+                    PrescriptionId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PatientPrescriptions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PatientPrescriptions_Patients_PatientPrescription",
+                        column: x => x.PatientPrescription,
+                        principalTable: "Patients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_PatientPrescriptions_Prescriptions_PrescriptionId",
+                        column: x => x.PrescriptionId,
+                        principalTable: "Prescriptions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_UserTypeId",
+                table: "Users",
+                column: "UserTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PatientPrescriptions_PatientPrescription",
+                table: "PatientPrescriptions",
+                column: "PatientPrescription");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PatientPrescriptions_PrescriptionId",
+                table: "PatientPrescriptions",
+                column: "PrescriptionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Prescriptions_BloodworkId",
+                table: "Prescriptions",
+                column: "BloodworkId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BloodworkTests_Bloodworks_BloodworkTest",
+                table: "BloodworkTests",
+                column: "BloodworkTest",
+                principalTable: "Bloodworks",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_UserTypes_UserTypeId",
+                table: "Users",
+                column: "UserTypeId",
+                principalTable: "UserTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
