2b50fc6faee1e116b6dc93da849896b933d0bbfb
Adam Sheppard <t7.shab@gmail.com>
2020-12-02 15:15:23 +0000

    Very basic user registration backend implementation
    Needs validation and to be called from the front end
    
    Resolves #20

--- AdvancedAnalysisDesign/Data/UserService.cs
+++ AdvancedAnalysisDesign/Data/UserService.cs
@@ -13,13 +13,37 @@ namespace AdvancedAnalysisDesign.Data
         {
             _contextFactory = contextFactory;
         }
-        
-        public async Task<User> GetUserAsync()
+
+        public async Task<UserDetail> RegisterUserDetails(UserDetail userDetail)
         {
             using (var context = _contextFactory.CreateDbContext())
             {
-                return await context.Users.Where(x => x.Username == "adam").FirstOrDefaultAsync();
+                await context.UserDetails.AddAsync(userDetail);
+                return userDetail;
             }
-        } 
+        }
+        
+        public async Task<User> RegisterUser(User user, UserDetail userDetails)
+        {
+            using (var context = _contextFactory.CreateDbContext())
+            {
+                user.UserDetail = userDetails;
+                await context.Users.AddAsync(user);
+                return user;
+            }
+        }
+
+        public async Task RegisterPatient(UserDetail userDetailsToRegister, User userToRegister, Patient patientToRegister)
+        {
+            var userDetails = await RegisterUserDetails(userDetailsToRegister);
+            var user = await RegisterUser(userToRegister, userDetails);
+
+            patientToRegister.User = user;
+            
+            using (var context = _contextFactory.CreateDbContext())
+            {
+                await context.Patients.AddAsync(patientToRegister);
+            }
+        }
     }
 }
--- AdvancedAnalysisDesign/Models/UserDetail.cs
+++ AdvancedAnalysisDesign/Models/UserDetail.cs
@@ -8,7 +8,6 @@ namespace AdvancedAnalysisDesign.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
-        public string EmailAddress { get; set; }
         public DateTimeOffset DateOfBirth { get; set; }
     }
 }
