94ba364f1a1b353da5c47b6b2bb0c99c66c58de3
Adam Sheppard <t7.shab@gmail.com>
2020-12-06 18:43:00 +0000

    Migration

---
+++ AdvancedAnalysisDesign/Migrations/20201206184235_UserDetails-ForeignKeys.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AdvancedAnalysisDesign.Migrations
+{
+    public partial class UserDetailsForeignKeys : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_UserDetails_UserDetailId",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserDetailId",
+                table: "Users",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_UserDetails_UserDetailId",
+                table: "Users",
+                column: "UserDetailId",
+                principalTable: "UserDetails",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_UserDetails_UserDetailId",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserDetailId",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_UserDetails_UserDetailId",
+                table: "Users",
+                column: "UserDetailId",
+                principalTable: "UserDetails",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
