dc915c4061af9374305140f34c508bd1065357bc
Adam Sheppard <t7.shab@gmail.com>
2021-02-01 14:09:06 +0000

    Login now works using blazor (#94)
    -Added functions to write cookies to store login information and wired up login to use them
    -Logout has been added - blank page that runs the logout function
    
    Resolves #77
    Resolves #81
    
    Co-authored-by: Leon Winstanley <43961881+LeonWinstanley@users.noreply.github.com>

--- AdvancedAnalysisDesign/Migrations/20210201135439_remove-duplicate-phone-number-User-is-IdentityUser.Designer.cs
+++ AdvancedAnalysisDesign/Migrations/20210201135439_remove-duplicate-phone-number-User-is-IdentityUser.Designer.cs
@@ -4,14 +4,16 @@ using AdvancedAnalysisDesign;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AdvancedAnalysisDesign.Migrations
 {
     [DbContext(typeof(AADContext))]
-    partial class AADContextModelSnapshot : ModelSnapshot
+    [Migration("20210201135439_remove-duplicate-phone-number-User-is-IdentityUser")]
+    partial class removeduplicatephonenumberUserisIdentityUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- AdvancedAnalysisDesign/Services/UserService.cs
+++ AdvancedAnalysisDesign/Services/UserService.cs
@@ -1,40 +1,73 @@
 using System;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using AdvancedAnalysisDesign.Models;
 using AdvancedAnalysisDesign.Models.Database;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.JSInterop;
+using Microsoft.VisualStudio.Web.CodeGeneration.DotNet;
+using MudBlazor;
 
 namespace AdvancedAnalysisDesign.Services
 {
     public class UserService
     {
         private readonly AADContext _context;
+        private readonly SignInManager<User> _signInManager;
+        private readonly UserManager<User> _userManager;
+        private readonly ISnackbar _snackbar;
+        private readonly NavigationManager _navigationManager;
+        private readonly CookieAuthenticationOptions _cookieAuthenticationOptions;
+        private readonly IHostEnvironmentAuthenticationStateProvider _hostAuthentication;
+        private readonly IJSRuntime _jsRuntime;
+        private readonly PasswordHasher<User> _passwordHasher;
         
-        public UserService(AADContext context)
+        public UserService(UserManager<User> userManager,
+            SignInManager<User> signInManager,
+            AADContext context,
+            ISnackbar snackbar,
+            NavigationManager navigationManager,
+            IOptionsMonitor<CookieAuthenticationOptions> cookieAuthenticationOptionsMonitor,
+            IHostEnvironmentAuthenticationStateProvider hostAuthentication,
+            IJSRuntime jsRuntime,
+            PasswordHasher<User> passwordHasher)
         {
+            _userManager = userManager;
+            _signInManager = signInManager;
             _context = context;
+            _snackbar = snackbar;
+            _navigationManager = navigationManager;
+            _hostAuthentication = hostAuthentication;
+            _jsRuntime = jsRuntime;
+            _cookieAuthenticationOptions = cookieAuthenticationOptionsMonitor.Get("MyScheme");
+            _passwordHasher = passwordHasher;
         }
 
         public async Task ForgotPasswordUpdate(ForgotPasswordPayload forgotPayload)
         {
-            var result = await _context.Users.SingleOrDefaultAsync(u => u.EmailAddress == forgotPayload.EmailAddress);
+            var result = await _context.Users.SingleOrDefaultAsync(u => u.Email == forgotPayload.EmailAddress);
 
             if (result != null)
             {
-                result.Password = forgotPayload.Password;
+                result.PasswordHash = forgotPayload.Password;
                 await _context.SaveChangesAsync();
             }
         }
 
-        public async Task<UserDetail> RegisterUserDetails(RegistrationPayload regPayload)
+        public UserDetail RegisterUserDetails(RegistrationPayload regPayload)
         {
             var userDetail = new UserDetail
             {
                 FirstName = regPayload.FirstName,
                 LastName = regPayload.LastName,
                 DateOfBirth = (System.DateTimeOffset)regPayload.DateOfBirth,
-                PhoneNumber = regPayload.PhoneNumber
             };
             
             return userDetail;
@@ -42,20 +75,22 @@ namespace AdvancedAnalysisDesign.Services
         
         public async Task<User> RegisterUser(RegistrationPayload regPayload)
         {
-            var userDetails = await RegisterUserDetails(regPayload);
+            var userDetails = RegisterUserDetails(regPayload);
             
-            if (_context.Users.Where(u => u.EmailAddress == regPayload.EmailAddress).Any())
+            if (_context.Users.Where(u => u.Email == regPayload.EmailAddress).Any())
             {
                 throw new ApplicationException("Email address is already in use!");
             }
             
             var user = new User
             {
-                EmailAddress = regPayload.EmailAddress,
-                Password = BCrypt.Net.BCrypt.HashPassword(regPayload.Password),
                 UserDetail = userDetails,
-                UserType = regPayload.UserType
+                Email = regPayload.EmailAddress,
+                UserName = regPayload.EmailAddress,
+                PhoneNumber = regPayload.PhoneNumber
             };
+            
+            var result = await _userManager.CreateAsync(user, regPayload.Password);
 
             return user; // Depending other user types are implemented, this may need changing. Currently standalone users cannot be registered.
         }
@@ -75,10 +110,56 @@ namespace AdvancedAnalysisDesign.Services
             await _context.SaveChangesAsync();
         }
 
-        public async Task<bool> Login(string email, string password)
+        public async Task Login(string email, string password)
         {
-            var user = await _context.Users.FirstOrDefaultAsync(u => u.EmailAddress == email);
-            return user != null && BCrypt.Net.BCrypt.Verify(password, user.Password);
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.Email == email);
+
+            if (_passwordHasher.VerifyHashedPassword(user, user.PasswordHash, password) != PasswordVerificationResult.Success)
+            {
+                _snackbar.Add("Login was unsuccessful. Please try again.", Severity.Error, config => { config.ShowCloseIcon = false; });
+                return;
+            }
+
+            await SignInAsync(user);
+        
+            _snackbar.Add("Login successful!", Severity.Success, config => { config.ShowCloseIcon = false; });
+            _navigationManager.NavigateTo("/");
+        }
+        
+        public async Task Logout()
+        {
+            await SignOutAsync();
+        
+            _snackbar.Add("Logout successful!", Severity.Success, config => { config.ShowCloseIcon = false; });
+            _navigationManager.NavigateTo("/");
+        }
+        
+        private async Task SignInAsync(User user)
+        {
+            var principal = await _signInManager.CreateUserPrincipalAsync(user);
+
+            var identity = new ClaimsIdentity(
+                principal.Claims,
+                "MyScheme"
+            );
+            principal = new ClaimsPrincipal(identity);
+            _signInManager.Context.User = principal;
+            _hostAuthentication.SetAuthenticationState(Task.FromResult(new AuthenticationState(principal)));
+
+            // this is where we create a ticket, encrypt it, and invoke a JS method to save the cookie
+            var ticket = new AuthenticationTicket(principal, null, "MyScheme");
+            var value = _cookieAuthenticationOptions.TicketDataFormat.Protect(ticket);
+            await _jsRuntime.InvokeVoidAsync("blazorExtensions.WriteCookie", "BinaryBeastAuth", value, _cookieAuthenticationOptions.ExpireTimeSpan.TotalDays);
+        }
+        
+        private async Task SignOutAsync()
+        {
+            var principal = _signInManager.Context.User = new ClaimsPrincipal(new ClaimsIdentity());
+            _hostAuthentication.SetAuthenticationState(Task.FromResult(new AuthenticationState(principal)));
+
+            await _jsRuntime.InvokeVoidAsync("blazorExtensions.DeleteCookie", "BinaryBeastAuth");
+
+            await Task.CompletedTask;
         }
     }
 }
