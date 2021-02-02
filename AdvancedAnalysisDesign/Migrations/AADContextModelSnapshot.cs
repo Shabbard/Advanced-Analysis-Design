65df190c3572ac43b45186b6c68d4c79945a4fb4
Adam Sheppard <t7.shab@gmail.com>
2021-02-02 17:28:30 +0000

    Update to remove need for IndentityHostingStartup (#110)

--- AdvancedAnalysisDesign/Migrations/AADContextModelSnapshot.cs
+++ AdvancedAnalysisDesign/Migrations/AADContextModelSnapshot.cs
@@ -315,12 +315,10 @@ namespace AdvancedAnalysisDesign.Migrations
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityUserLogin<string>", b =>
                 {
                     b.Property<string>("LoginProvider")
-                        .HasMaxLength(128)
-                        .HasColumnType("nvarchar(128)");
+                        .HasColumnType("nvarchar(450)");
 
                     b.Property<string>("ProviderKey")
-                        .HasMaxLength(128)
-                        .HasColumnType("nvarchar(128)");
+                        .HasColumnType("nvarchar(450)");
 
                     b.Property<string>("ProviderDisplayName")
                         .HasColumnType("nvarchar(max)");
@@ -357,12 +355,10 @@ namespace AdvancedAnalysisDesign.Migrations
                         .HasColumnType("nvarchar(450)");
 
                     b.Property<string>("LoginProvider")
-                        .HasMaxLength(128)
-                        .HasColumnType("nvarchar(128)");
+                        .HasColumnType("nvarchar(450)");
 
                     b.Property<string>("Name")
-                        .HasMaxLength(128)
-                        .HasColumnType("nvarchar(128)");
+                        .HasColumnType("nvarchar(450)");
 
                     b.Property<string>("Value")
                         .HasColumnType("nvarchar(max)");
