20c2cd15e4225c961f635c626b6cdf897cd6b4d2
Alex <34196618+KaneyCain@users.noreply.github.com>
2021-02-02 10:35:17 +0000

    Blood work models (#96)
    * Updated Models 
    
    Updated Models as per team discussion
    
    * Updated Migrations with new models
    
    * Remove Expletives
    
    Co-authored-by: Leon Winstanley <43961881+LeonWinstanley@users.noreply.github.com>
    Co-authored-by: Adam Sheppard <t7.shab@gmail.com>

--- AdvancedAnalysisDesign/Models/Database/BloodworkTest.cs
+++ AdvancedAnalysisDesign/Models/Database/BloodworkTest.cs
@@ -2,6 +2,6 @@ namespace AdvancedAnalysisDesign.Models.Database
 {
     public class BloodworkTest
     {
-        
+        //*List of loads of tests pending nigel talk* - Leon
     }
 }
--- AdvancedAnalysisDesign/Models/Database/PatientBloodworkTest.cs
+++ AdvancedAnalysisDesign/Models/Database/PatientBloodworkTest.cs
@@ -3,7 +3,7 @@ namespace AdvancedAnalysisDesign.Models.Database
     public class PatientBloodworkTest
     {
         public int Id { get; set; }
-        public string Results { get; set; }
-        public bool Completed { get; set; }
-      }
+        public string Result { get; set; }
+        public string TestType { get; set; }
+    }
 }
