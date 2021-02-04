5b0a52894512e92bca7faf39ed9f73827c7f18c1
Alex <34196618+KaneyCain@users.noreply.github.com>
2021-02-04 19:56:52 +0000

    Added New GP Model (#115)
    * Added New GP Model
    
    - Added New GP Model
    - Migrated into DB 
    - Linked with users and patients
    
    * Updated GP table
    
    * Update GP Name
    
    * GP to GP
    
    Co-authored-by: Adam Sheppard <t7.shab@gmail.com>

---
+++ AdvancedAnalysisDesign/Migrations/20210204195210_AddedGPModel.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AdvancedAnalysisDesign.Migrations
+{
+    public partial class AddedGPModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GeneralPractitionerId",
+                table: "Patients",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "GeneralPractitioners",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    EmergencyContact = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    OfficeNumber = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GeneralPractitioners", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GeneralPractitioners_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Patients_GeneralPractitionerId",
+                table: "Patients",
+                column: "GeneralPractitionerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GeneralPractitioners_UserId",
+                table: "GeneralPractitioners",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Patients_GeneralPractitioners_GeneralPractitionerId",
+                table: "Patients",
+                column: "GeneralPractitionerId",
+                principalTable: "GeneralPractitioners",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Patients_GeneralPractitioners_GeneralPractitionerId",
+                table: "Patients");
+
+            migrationBuilder.DropTable(
+                name: "GeneralPractitioners");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Patients_GeneralPractitionerId",
+                table: "Patients");
+
+            migrationBuilder.DropColumn(
+                name: "GeneralPractitionerId",
+                table: "Patients");
+        }
+    }
+}
