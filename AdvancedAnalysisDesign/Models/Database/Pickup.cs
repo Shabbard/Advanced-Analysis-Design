32cbc5247f7274674abeafb95fb8bc2c3bfe691e
Leon Winstanley <43961881+LeonWinstanley@users.noreply.github.com>
2021-02-09 16:25:34 +0000

    Updated models and added migrations. (#164)

---
+++ AdvancedAnalysisDesign/Models/Database/Pickup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdvancedAnalysisDesign.Models.Database
+{
+    public class Pickup
+    {
+        public int Id { get; set; }
+        public bool IsPickedUp { get; set; }
+        public bool IsPrepared { get; set; }
+        public DateTimeOffset DatePickedUp { get; set; }
+    }
+}
