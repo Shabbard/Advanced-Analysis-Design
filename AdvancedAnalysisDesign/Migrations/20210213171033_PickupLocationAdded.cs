15c7fed152fb1fe3445b4c567718d6b0fd9597d7
Alex <34196618+KaneyCain@users.noreply.github.com>
2021-02-13 17:15:45 +0000

    Added Pickup Location to a Medication (#189)

---
+++ AdvancedAnalysisDesign/Migrations/20210213171033_PickupLocationAdded.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AdvancedAnalysisDesign.Migrations
+{
+    public partial class PickupLocationAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MedicalInstitutionId",
+                table: "Pickups",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pickups_MedicalInstitutionId",
+                table: "Pickups",
+                column: "MedicalInstitutionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pickups_MedicalInstitutions_MedicalInstitutionId",
+                table: "Pickups",
+                column: "MedicalInstitutionId",
+                principalTable: "MedicalInstitutions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pickups_MedicalInstitutions_MedicalInstitutionId",
+                table: "Pickups");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Pickups_MedicalInstitutionId",
+                table: "Pickups");
+
+            migrationBuilder.DropColumn(
+                name: "MedicalInstitutionId",
+                table: "Pickups");
+        }
+    }
+}
