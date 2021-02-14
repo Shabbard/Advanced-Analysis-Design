0ce552a9014f6eb61b1355b8393de9101a1441a0
Adam Sheppard <t7.shab@gmail.com>
2021-02-14 15:57:17 +0000

    Gp upload page (#190)
    * Input UI added table needed
    
    * Rounded Pages and Centralised Button
    
    * Table Works with data input validation needed
    
    * Connected to Patient Search Page
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * GP upload page mostly works
    
    * Removed usings in UserDetail.cs
    
    Co-authored-by: Alex <cainalex33@gmail.com>

--- AdvancedAnalysisDesign/Models/Database/PatientMedication.cs
+++ AdvancedAnalysisDesign/Models/Database/PatientMedication.cs
@@ -9,7 +9,7 @@ namespace AdvancedAnalysisDesign.Models.Database
         public int Id { get; set; }
         public Medication Medication { get; set; }
         public bool BloodworkRequired { get; set; }
-        public ICollection<PatientBloodwork> PatientBloodworks { get; set; }
+        public List<PatientBloodwork> PatientBloodworks { get; set; }
         public TimeSpan DateIntervalOfBloodworkRenewal { get; set; }
         public Pickup Pickup { get; set; }
         
