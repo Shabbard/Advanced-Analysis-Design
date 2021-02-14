1ffc54518c538121d91dadbbd5be222785d1e074
Leon Winstanley <leon.winstanley2017@my.ntu.ac.uk>
2021-02-14 16:09:28 +0000

    Added medicalInt to patient.

---
+++ AdvancedAnalysisDesign/Migrations/20210214160711_AddMedicalIntToPatient.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AdvancedAnalysisDesign.Migrations
+{
+    public partial class AddMedicalIntToPatient : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MedicalInstitutionId",
+                table: "Patients",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Patients_MedicalInstitutionId",
+                table: "Patients",
+                column: "MedicalInstitutionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Patients_MedicalInstitutions_MedicalInstitutionId",
+                table: "Patients",
+                column: "MedicalInstitutionId",
+                principalTable: "MedicalInstitutions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Patients_MedicalInstitutions_MedicalInstitutionId",
+                table: "Patients");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Patients_MedicalInstitutionId",
+                table: "Patients");
+
+            migrationBuilder.DropColumn(
+                name: "MedicalInstitutionId",
+                table: "Patients");
+        }
+    }
+}
