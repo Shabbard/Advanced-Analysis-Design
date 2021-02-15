2b13a0a731c85056bd83d5ab74a3aee48f6bd960
Leon Winstanley <43961881+LeonWinstanley@users.noreply.github.com>
2021-02-15 14:06:51 +0000

    Added migrations to patient medication. (#195)

--- AdvancedAnalysisDesign/Models/Database/PatientMedication.cs
+++ AdvancedAnalysisDesign/Models/Database/PatientMedication.cs
@@ -10,6 +10,8 @@ namespace AdvancedAnalysisDesign.Models.Database
         public Medication Medication { get; set; }
         public bool BloodworkRequired { get; set; }
         public List<PatientBloodwork> PatientBloodworks { get; set; }
+        public TimeSpan DateIntervalOfPickup { get; set; }
+        public DateTimeOffset DateOfMedicationStart { get; set; }
         public TimeSpan DateIntervalOfBloodworkRenewal { get; set; }
         public Pickup Pickup { get; set; }
         
